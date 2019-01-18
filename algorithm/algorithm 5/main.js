a0280c5498a43f1191ecef2318a44d021410aa0c
Moises Valdez <moisesvaldez075@gmail.com>
2019-01-17 20:28:55 -0800

    did some more algorithms

---
+++ algorithm/algorithm 5/main.js
@@ -0,0 +1,15 @@
+function factorial(num) {
+    let product = 1;
+
+
+    for (let i = 1; i <= num; i++) {
+        product *= i;
+    }
+
+    return product;
+
+
+
+}
+
+console.log(factorial(4));
